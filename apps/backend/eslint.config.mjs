2ebff82a0895d99d07097fffd6946818597c3693
Gabriel Amorim <gabrielrra.dev@gmail.com>
2025-03-07 10:46:01 -0300

    feat: configure TypeORM with PostgreSQL and update ESLint/Prettier settings

--- apps/backend/eslint.config.mjs
+++ apps/backend/eslint.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default tseslint.config(
   {
@@ -10,7 +10,6 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -29,7 +28,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
+  eslintConfigPrettier,
 );
